12d2c4e75324b16a01e901cdc734d70def575770
Derek Howes <33637595+Dhowes24@users.noreply.github.com>
2022-11-21 22:21:04 -0500

    Initial Commit

---
+++ PaletteMimic/PaletteMimicApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaletteMimicApp.swift
+//  PaletteMimic
+//
+//  Created by Derek Howes on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct PaletteMimicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
